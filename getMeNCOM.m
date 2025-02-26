08dce8516c226b866fb09f5087c530ccdf708479
cathayanBoris <yan_junnan@hotmail.com>
2025-02-26 11:04:23 -0500

    2/26/25 +getMeNCOM.m

--- getMeNCOM.m
+++ getMeNCOM.m
@@ -1,4 +1,4 @@
-function [idx] = getNCOMSerial(degreeLon,degreeLat)
+function [idx] = getMeNCOM(degreeLon,degreeLat)
 load ncom_grid
 
 dst=sqrt((degreeLat-lat).^2+(degreeLon-lon).^2);
@@ -7,5 +7,4 @@ idx=idx(1);
 % TS.NCOMLAT=lat(idx);
 % TS.NCOMLON=lon(idx);
 % TS.DEPTH=h(idx);
-
-sprintf('%5.5i',idx)
+% sprintf('%5.5i',idx)
