32c7245c7a0ccede24f9fe25ef194482e8cc8a11
cathayanBoris <yan_junnan@hotmail.com>
2025-04-14 14:25:58 -0400

    4/14/25 sw_f, narrower derivative

--- getMeBathymetryTools/getMeBeta.m
+++ getMeBathymetryTools/getMeBeta.m
@@ -7,7 +7,7 @@ catch
     try
         beta = (sw_f(latitude+0.001) - sw_f(latitude-0.001))/(0.002*pi*6371000/180);
     catch
-        beta = 2*omega*(sind(latitude+0.001) - sind(latitude-0.001)/(0.002*pi*6371000/180);
+        beta = 2*omega*(sind(latitude+0.001) - sind(latitude-0.001))/(0.002*pi*6371000/180);
         % beta = 2*omega*cosd(latitude)/6371000;
     end
 end
