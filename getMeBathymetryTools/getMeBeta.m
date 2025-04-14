4d40ba344a0b1844bb84184bbf6863cc76b442f3
cathayanBoris <yan_junnan@hotmail.com>
2025-04-14 10:55:25 -0400

    4/14/25 sw_f, smaller dt

--- getMeBathymetryTools/getMeBeta.m
+++ getMeBathymetryTools/getMeBeta.m
@@ -2,7 +2,12 @@ function beta = getMeBeta(latitude)
 % beta = df/dy
 omega=7.292e-5;   %% Earth's rotation rate(s-1)   A.E.Gill p.597
 try
-beta = (gsw_f(latitude+0.01) - gsw_f(latitude-0.01))/(0.02*pi*6371000/180);
+    beta = (gsw_f(latitude+0.001) - gsw_f(latitude-0.001))/(0.002*pi*6371000/180);
 catch
-beta = 2*omega*(sind(latitude+0.01) - sind(latitude-0.01))/(0.02*pi*6371000/180);
+    try
+        beta = (sw_f(latitude+0.001) - sw_f(latitude-0.001))/(0.002*pi*6371000/180);
+    catch
+        beta = 2*omega*(sind(latitude+0.001) - sind(latitude-0.001)/(0.002*pi*6371000/180);
+        % beta = 2*omega*cosd(latitude)/6371000;
+    end
 end
