5ce6cfc9a21870293e7dd119b917ab6caf48174f
cathayanBoris <yan_junnan@hotmail.com>
2025-04-14 11:00:11 -0400

    4/14/25 UI redesign, smoothing sensitive

--- getMeRayPathTools/getMeSlownessCurve.m
+++ getMeRayPathTools/getMeSlownessCurve.m
@@ -1,16 +1,18 @@
-[~,~,ztopo] = getMeTopo(-91.5,-87.5,25.8,27.8);
-[xtopo,ytopo,ztopoS] = getMeSmoothed(30000,-91.5,-87.5,25.8,27.8);
-wavelengths = 1000*[25:25:600];
-
 % loop example
 load()
 
+[~,~,ztopo] = getMeTopo(-91.5,-87.5,25.8,27.8);
+[xtopo,ytopo,ztopoS] = getMeSmoothed(smoothing*1000,-91.5,-87.5,25.8,27.8);
+
 [pathCg,pathCgx,pathCgy] = getMeGroupVelocities(startT,pathK,pathL,pathEi);
 %%
+cmap = flipud(parula);
+
 for q = 1:abs(1/dt_hr):length(pathLon)
+    wavelengths = 1000*[100:50:600];
+
     figure(11)
-    % clf
-    colormap default
+    % colormap default
     beta=2.0E-11;
 
     NB = pathEi(q,7);
@@ -26,8 +28,8 @@ for q = 1:abs(1/dt_hr):length(pathLon)
     lat = pathEi(q,8);
     f=sw_f(pathLat(q));
 
-    inc = 1000;
-    Kbox=2e-4;     % big box for wavelengths as short as 62.8 km
+    inc = 500;
+    Kbox=1.5e-4;     % big box for wavelengths as short as 62.8 km
     Kmid=Kbox/4;   % coarse steps for Kbox>Kmid; fine steps for K0<Kmid
 
     ll1 = [-Kbox:Kbox/inc:-Kmid-Kbox/inc];
@@ -79,63 +81,88 @@ for q = 1:abs(1/dt_hr):length(pathLon)
         difference(jl) = min(abs(rhs-lhs));
         iwant = find(xx==min(xx));   % iwant = index of kk that best fits this ll
         kl(jl) = kk(iwant);          % kl is k(l,omega,Ei) for jl outer loop
-        if difference(jl) > 1e-2 %1e-2  % was 1e-7 for KD's arrangement, factor Kb smaller
+        if difference(jl) > 5e-3 %1e-2  % was 1e-7 for KD's arrangement, factor Kb smaller
             kl(jl) = NaN;
         end %if
     end % for jl, outer small suite of ll's
     ang=1:89;
     sinang=sind(ang); cosang=cosd(ang);
+
     figure(11)
-    subplot(1,2,1)
+    clf
+    subplot(1,2,1) %%%
     v = -3300:100:0;
-    contour(xtopo,ytopo,ztopo',v,'-.','ShowText','on');
     hold on
-    contour(xtopo,ytopo,ztopoS',v,'k','ShowText','off');
-    contour(xtopo,ytopo,ztopo',-0.1:0.1:0.1,'k');
-    scatter(pathLon,pathLat,8,'o','MarkerEdgeColor',"#ff7d9d");
+    [~,zzzz]=contour(xtopo,ytopo,ztopoS',v,'k','ShowText','on','DisplayName','Topography');
+    contour(xtopo,ytopo,ztopo',v,'-.','ShowText','off','EdgeAlpha',0.5);
+    % contour(xtopo,ytopo,ztopo',-0.1:0.1:0.1,'k');
+    aaaa = scatter(pathLon,pathLat,10,'filled','o','MarkerEdgeColor',"#ff7d9d",'MarkerFaceColor',"#ff7d9d");
     scatter(pathLon(q),pathLat(q),5,'o','MarkerEdgeColor',"black","LineWidth",5);
-    quiver(pathLon(q),pathLat(q),pathCgx(q),pathCgy(q),0.1,'k',"MaxHeadSize",100,"LineWidth",3);
+    magnitude = abs(pathEi(q,2) + 1i*pathEi(q,3));
+    cccc = quiver(pathLon(q)-pathEi(q,2)/magnitude*0.05,pathLat(q)-pathEi(q,3)/magnitude*0.05,pathEi(q,2)/magnitude*0.14,pathEi(q,3)/magnitude*0.14,"MaxHeadSize",0.5,"LineWidth",3,"Color","#ffb300"); % topo gradient
+    magnitude = abs(pathCgx(q) + 1i*pathCgy(q));
+    bbbb = quiver(pathLon(q),pathLat(q),pathCgx(q)./magnitude.*0.1,pathCgy(q)./magnitude.*0.1,'k',"MaxHeadSize",5,"LineWidth",3);
+    text(pathLon(q)+pathCgx(q)./magnitude.*0.1+0.03, pathLat(q)+pathCgy(q)./magnitude.*0.1+0.01, ['Cg= ', num2str(pathCg(q),4),' m/s'])
     axis equal
     axis([pathLon(q)-0.4 pathLon(q)+0.4 pathLat(q)-0.4 pathLat(q)+0.4])
-    text(pathLon(q)+0.01, pathLat(q), ['Cg= ', num2str(pathCg(q),4),' m/s'])
     hold off
     grid on
     ax=gca;
-    ax.FontSize = 15;
-    xlabel('Longitude E',"FontSize",20)
-    ylabel('Latitude N',"FontSize",20)
-    title(['Location at ',num2str((q-1)*dt_hr), ' hr, 20km smoothed'],"FontSize",20)
+    ax.FontSize = 12;
+    legend(ax,[zzzz bbbb aaaa cccc],'Experiment Topography','Energy Propagation','Ray path','Topo. Gradient ∇h')
+    xlabel('Longitude ^oE',"FontSize",20)
+    ylabel('Latitude ^oN',"FontSize",20)
+    title(gca,sprintf(['Location at %ghr, %0.0fkm smoothed'],(q-1)*dt_hr,smoothing),"FontSize",20)
     
-    subplot(1,2,2)
+    subplot(1,2,2) %%%
     % figure(11)    % dispersion [k,l] curves
-    plot(kl,ll,'b.');    % y=ll vs x=kl
+    colorIndex = 0.002*pi./[abs(kl+1i.*ll)];
+    zzzz = scatter(kl,ll,30,colorIndex,'filled');    % y=ll vs x=kl
+    clim([0 wavelengths(end)/1000])
     hold on
-    bbbb = quiver(pathK(q),pathL(q),pathCgx(q),pathCgy(q),"off",'k',"LineWidth",3); % Cg
-    cccc = quiver(0,0,pathEi(q,2),pathEi(q,3),"off","LineWidth",3,"Color","#ffb300"); % topo gradient
+    magnitude = abs(pathCgx(q) + 1i*pathCgy(q));
+    bbbb = quiver(pathK(q),pathL(q),pathCgx(q)./magnitude.*5e-5,pathCgy(q)./magnitude.*5e-5,'k',"MaxHeadSize",1,"LineWidth",3); % Cg
+    magnitude = abs(pathEi(q,2) + 1i*pathEi(q,3));
+    cccc = quiver(-pathEi(q,2)/magnitude*Kbox*0.7,-pathEi(q,3)/magnitude*Kbox*0.7,pathEi(q,2)/magnitude*Kbox*1.5,pathEi(q,3)/magnitude*Kbox*1.5,"MaxHeadSize",0.25,"LineWidth",3,"Color","#ffb300"); % topo gradient
+    magnitude = abs(pathK(q) + 1i*pathL(q));
     aaaa = quiver(0,0,pathK(q),pathL(q),"off","Color","r","LineWidth",5,"MaxHeadSize",3); % K
-
+    
+    wavelengths = [wavelengths 2*pi/abs(pathK(q)+1i*pathL(q))];
     for i = 1:length(wavelengths) %subsample KK0's by factor of 10 to plot K0 circles
         K0=2*pi/wavelengths(i);
         kcirc=K0*cosang;
         lcirc=K0*sinang;
-        plot(kcirc,lcirc,'m-.', kcirc,-lcirc,'m-.',...
-            -kcirc,lcirc,'m-.',-kcirc,-lcirc,'m-.')
+        if i == length(wavelengths)
+            getMeColorfulPlot(kcirc,lcirc,0.002*pi./abs(kcirc+1i*lcirc),1,'--',1);
+            getMeColorfulPlot(-kcirc,lcirc,0.002*pi./abs(kcirc+1i*lcirc),1,'--',1);
+            getMeColorfulPlot(kcirc,-lcirc,0.002*pi./abs(kcirc+1i*lcirc),1,'--',1);
+            cirkle = getMeColorfulPlot(-kcirc,-lcirc,0.002*pi./abs(kcirc+1i*lcirc),1,'--',1);
+        else
+        % circle = plot(kcirc,lcirc,'-.', kcirc,-lcirc,'-.',...
+        %     -kcirc,lcirc,'-.',-kcirc,-lcirc,'-.','Color',[1 0 1 0.3]);
+        end
     end
     theta = acosd((pathK(q)*pathEi(q,2)+pathL(q)*pathEi(q,3))/(pathK0(q)*sqrt(pathEi(q,2)^2+pathEi(q,3)^2)));
-    text(-18e-5, 8e-5,['theta (^o)= ', num2str(theta,4')])
-    text(-18e-5, 10e-5,['hx= ', num2str(hx,4'), ', hy= ', num2str(hy,4)])
-    text(-18e-5, 12e-5, ['h (m)= ', num2str(h,4)])
-    % text(-18e-5, 14e-5, ['K0= ', num2str(pathK0(q),4)])
-    text(-18e-5, 14e-5, ['Wavelengh (km)= ', num2str(2*pi/1000/pathK0(q),4)])
-    text(-18e-5, 16e-5, ['Wavelength Cicles (km): 25:25:600'])
-    legend([aaaa bbbb cccc],"Wavenumber K0","Group Velocity","Bathy. Gradient")
+    % text(-18e-5, 8e-5,['Cross Product Angle (^o)= ', num2str(theta,4')])
+    % text(-18e-5, 10e-5,['hx= ', num2str(hx,4'), ', hy= ', num2str(hy,4)])
+    % text(-18e-5, 12e-5, ['h (m)= ', num2str(h,4)])
+    text(mean(kcirc), -mean(kcirc), ['Wavelength ', num2str(2*pi/1000/pathK0(q),4) 'km'],"VerticalAlignment","top","HorizontalAlignment","left")
+    % text(-18e-5, 16e-5, ['Wavelength Circles (km): 25:25:600'])
+    legend([zzzz aaaa bbbb cccc cirkle],"Slowness Curve", ... 
+        ' Wavenumber K',"Group Velocity","Topo. Gradient","K Isocontour")
     axis([-Kbox +Kbox -Kbox +Kbox])
+    cbar = colorbar(gca,"south"); xlabel(cbar,'Wavelength (km)')
     axis square
-    ax=gca;
-    ax.FontSize = 15;
-    title(['Period = ', num2str(startT),' days'],"FontSize",20)
-    xlabel('k (1/m)',"FontSize",20); ylabel('l (1/m)',"FontSize",20);
+    ax = gca;
+    colormap(gca,cmap)
+    ax.FontSize = 12;
+    title(['Wave Period = ', num2str(startT),' days'],"FontSize",20)
+    xlabel('k (radian m^{-1})',"FontSize",20); ylabel('l (radian m^{-1})',"FontSize",20);
     grid on
     hold off
-    % pause(.01);
+
+    %%% generate GIF
+    % exportgraphics(gcf,'slowessDemo.gif','Append',true);
+
+    pause(.01);
 end
