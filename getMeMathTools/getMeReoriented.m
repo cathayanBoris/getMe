eed4458bdb21b554ab823d17ad903a2215c58acd
cathayanBoris <yan_junnan@hotmail.com>
2025-05-05 12:09:50 -0400

    5/5/25/ add axis ratio output

--- getMeMathTools/getMeReoriented.m
+++ getMeMathTools/getMeReoriented.m
@@ -1,12 +1,14 @@
-function [reorientedComplexTSOutput,radianAppliedToWin] = getMeReoriented(complexTS,mode)
+function [reorientedComplexTSOutput,radianAppliedToWin,ratio] = getMeReoriented(complexTS,mode)
 
 % modes:
 % Meanflow
-% Variance
-% Ratio - pretty much the same as Variance
+% Variance - u> direction of biggest variance
+% Ratio - 'most elongated' pretty much the same as Variance
+
+division  = 7200;
 
 if exist('mode','var') == 0
-    error('Bruh you forgot to select your mode of rotation')
+    error('Bruh you forgot to select your mode of rotation.')
 end
 
 % point positive u in the direction of...
@@ -16,7 +18,7 @@ end
 
 if mode(2:4) == 'ari'  % largest variance
     steve = 0;
-    for theta = -pi/2:2*pi/7200:pi/2
+    for theta = -pi/2:2*pi/division:pi/2
         rotationAttempt = complexTS(:,:).*exp(1i*theta);
         if std(real(rotationAttempt),'omitmissing')>steve
             steve = std(real(rotationAttempt),'omitmissing');
@@ -27,7 +29,7 @@ end
 
 if mode(2:4) == 'ati'  % largest variance ratio
     stever = 1;
-    for theta = -pi/2:2*pi/7200:pi/2
+    for theta = -pi/2:2*pi/division:pi/2
         rotationAttempt = complexTS(:,:).*exp(1i*theta);
         if std(real(rotationAttempt),'omitmissing')/std(imag(rotationAttempt),'omitmissing')>stever
             stever = std(real(rotationAttempt),'omitmissing')/std(imag(rotationAttempt),'omitmissing');
@@ -38,3 +40,4 @@ end
 
 % rotationRAD is the angle in radians applied on TS to align u> with x>
 reorientedComplexTSOutput = complexTS .* exp(1i * radianAppliedToWin);
+ratio = std(real(reorientedComplexTSOutput),'omitmissing')/std(imag(reorientedComplexTSOutput),'omitmissing');
