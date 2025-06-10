039ec8bcfe990786211162014ab2c38f1b7c796d
cathayanBoris <yan_junnan@hotmail.com>
2025-06-10 17:39:30 -0400

    6/10/25 detail adjustments

--- getMeMathTools/getMeReoriented.m
+++ getMeMathTools/getMeReoriented.m
@@ -5,7 +5,7 @@ function [reorientedComplexTSOutput,radianAppliedToWin,ratio] = getMeReoriented(
 % Variance - u> direction of biggest variance
 % Ratio - 'most elongated' pretty much the same as Variance
 
-division  = 7200;
+division  = 3600;
 
 if exist('mode','var') == 0
     error('Bruh you forgot to select your mode of rotation.')
@@ -18,26 +18,49 @@ end
 
 if mode(2:4) == 'ari'  % largest variance
     steve = 0;
-    for theta = -pi/2:2*pi/division:pi/2
+    radianAppliedToWin = 0;
+    for theta = -pi/2:pi/division:pi/2  %0.05 interval
         rotationAttempt = complexTS(:,:).*exp(1i*theta);
         if std(real(rotationAttempt),'omitmissing')>steve
             steve = std(real(rotationAttempt),'omitmissing');
             radianAppliedToWin=theta;
+        elseif std(real(rotationAttempt),'omitmissing')==steve
+            radianAppliedToWin=[radianAppliedToWin theta];
         end
     end
 end
 
 if mode(2:4) == 'ati'  % largest variance ratio
     stever = 1;
-    for theta = -pi/2:2*pi/division:pi/2
+    radianAppliedToWin = 0;
+    for theta = -pi/2:pi/division:pi/2  %0.05 interval
         rotationAttempt = complexTS(:,:).*exp(1i*theta);
         if std(real(rotationAttempt),'omitmissing')/std(imag(rotationAttempt),'omitmissing')>stever
             stever = std(real(rotationAttempt),'omitmissing')/std(imag(rotationAttempt),'omitmissing');
             radianAppliedToWin=theta;
+        elseif std(real(rotationAttempt),'omitmissing')/std(imag(rotationAttempt),'omitmissing')==stever
+            radianAppliedToWin=[radianAppliedToWin theta];
         end
     end
 end
 
 % rotationRAD is the angle in radians applied on TS to align u> with x>
+try % if there is only one winner
+    reorientedComplexTSOutput = complexTS .* exp(1i * radianAppliedToWin);
+    ratio = std(real(reorientedComplexTSOutput),'omitmissing')/std(imag(reorientedComplexTSOutput),'omitmissing');
+catch % if there are multiple winners
+    if std(abs(radianAppliedToWin)) == 0 || sum(radianAppliedToWin) == 0 % if they are exactly opposite angles
+        try
+            radianAppliedToWin = max(radianAppliedToWin(radianAppliedToWin<0));
+        catch
+            radianAppliedToWin = min(radianAppliedToWin(radianAppliedToWin>0));
+        end
+    else
+        [~,index] = min(abs(radianAppliedToWin));
+        radianAppliedToWin = radianAppliedToWin(index); % find the rotation of least effort
+
+    end
+end
 reorientedComplexTSOutput = complexTS .* exp(1i * radianAppliedToWin);
 ratio = std(real(reorientedComplexTSOutput),'omitmissing')/std(imag(reorientedComplexTSOutput),'omitmissing');
+end
--- getMeNCOMEnergies.m
+++ getMeNCOMEnergies.m
@@ -1,4 +1,4 @@
-function [EKE,EPE] = getMeNCOMEnergies(NCOMinput)
+function [EKE,EPE,Ua,Va] = getMeNCOMEnergies(NCOMinput)
 % NCOM input needs to be in char format
 fileName = ['NCOM\' NCOMinput '.mat'];
 load(fileName);
@@ -17,21 +17,46 @@ u100 = eval([NCOMinput '.u100']);
 v100 = eval([NCOMinput '.v100']);
 
 % EKE calculation
-u500a = u500 - nanmean(u500,3); v500a = v500 - nanmean(v500);
-u300a = u300 - nanmean(u300,3); v300a = v300 - nanmean(v300);
-u100a = u100 - nanmean(u100,3); v100a = v100 - nanmean(v100);
+u500a = u500 - nanmean(u500,3); v500a = v500 - nanmean(v500,3);
+u300a = u300 - nanmean(u300,3); v300a = v300 - nanmean(v300,3);
+u100a = u100 - nanmean(u100,3); v100a = v100 - nanmean(v100,3);
 
+Ua = 1/3 * (u100a + u300a + u500a);
+Va = 1/3 * (v100a + v300a + v500a);
 EKE = 1/6 * ((abs(u500a + 1i.*v500a)).^2 + (abs(u300a + 1i.*v300a)).^2 + (abs(u100a + 1i.*v100a)).^2);
+% EKE(Ua < 0) = -EKE(Ua < 0);
 
 % EPE calculation
+% isothermFlag = or(logical(t4 <= depth),isnan(t4));
+% refDepth = zeros([length(lat(:,1)) length(lon(1,:))]);
+% for ii = 1:length(lon(1,:))
+%     for jj = 1:length(lat(:,1))
+%         if sum(isothermFlag(jj,ii,:)) >= length(t4(1,1,:))
+%             refDepth(jj,ii) = depth(jj,ii);
+%         else
+%             refDepth(jj,ii) = mode(round(t4(jj,ii,~isothermFlag(jj,ii,:))));
+%         end
+%         % fprintf(1,'%d of %d at %d/%d \n',length(lat(:,1))*(ii-1)+jj,length(lat(:,1))*length(lon(1,:)),ii,jj)
+%     end
+% end
+% EPE = 0.5 * (NBh(-refDepth)).^2 .* (t4-refDepth).^2;
+% end
+
 isothermFlag = or(logical(t4 <= depth),isnan(t4));
+
 refDepth = zeros([length(lat(:,1)) length(lon(1,:))]);
 for ii = 1:length(lon(1,:))
     for jj = 1:length(lat(:,1))
-        if sum(isothermFlag(jj,ii,:)) >= length(t4(1,1,:))
+        if sum(isothermFlag(jj,ii,:)) >= length(t4(1,1,:)) % if all is problematic
             refDepth(jj,ii) = depth(jj,ii);
+        elseif sum(isothermFlag(jj,ii,:)) >= length(t4(1,1,:))/2  && sum(isothermFlag(jj,ii,:)) < length(t4(1,1,:)) % if more than half is problematic
+            % length(t4(1,1,:)) - sum(isothermFlag(jj,ii,:)) 
+            % length(t4(jj,ii,~isothermFlag(jj,ii,:)))
+            % figure(100)
+            % plot(t4(jj,ii,~isothermFlag(jj,ii)))
+            refDepth(jj,ii) = round(min(t4(jj,ii,~isothermFlag(jj,ii,:)),[],"all")); %
         else
-            refDepth(jj,ii) = mode(round(t4(jj,ii,~isothermFlag(jj,ii))));
+            refDepth(jj,ii) = mode(round(t4(jj,ii,~isothermFlag(jj,ii,:))));
         end
         % fprintf(1,'%d of %d at %d/%d \n',length(lat(:,1))*(ii-1)+jj,length(lat(:,1))*length(lon(1,:)),ii,jj)
     end
